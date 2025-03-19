2d389716b0a713bdb08ae27164249920b6bd8c7d
yamiz0 <ianlop090@gmail.com>
2025-03-18 20:00:46 -0400

    Biblioteca

---
+++ Biblioteca/src/biblioteca/Usuario.java
@@ -0,0 +1,84 @@
+package biblioteca;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+    private String endereco;
+
+    // construtor
+
+    public Usuario(int id, String nome, String email, String senha, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    // getters e setters
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+}
